3ae650e18c73c44e760b24846d584287fc24cb5d
yasser <yasser@yass>
2019-05-19 22:01:44 +0200

    Creación de la plantilla "temp-welcome.php".
    En esta plantilla se diseña el mensaje de bienvenida de la página web.

---
+++ php/templates/temp-welcome.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-xs-12 col-md-12 col-xl-12">
+        <div class="card" id="welcome">
+            <div class="card-body" id="body-welcome">
+                <h2 class="car-title" id="title-welcome">¿Quénes somos?</h2>
+                <p>Bienvenido a Melicar, líder en Melilla en alquiler de coches.
+                Melicar te garantiza un servicio insuperable, que busques coches económicos, familiares
+                o de alta gama para visitar nuestra ciudad, o bien furgonetas y vehículos comerciales.
+                Ahora tienes más razones que nunca para alquilar con Melicar.</p>
+            </div>
+        </div>
+    </div>
+</div>
