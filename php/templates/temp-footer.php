68f22879cfb857caca33b5375a59aa673bf6e429
yasser <yasser@yass>
2019-05-19 22:00:25 +0200

    Creación de la plantilla "temp-footer.php".
    En esta plantilla se diseña el footer de la página web.

---
+++ php/templates/temp-footer.php
@@ -0,0 +1,14 @@
+<footer class="row" id="page-footer">
+    <div class="col-xs-12 col-md-12 col-xl-4" id="p-footer1">
+        <p class="info-footer"><img src="../../images/email.svg" class="img-footer" id="icon1">melicar@gmail.com</p>
+    </div>
+    
+    <div class="col-xs-12 col-md-12 col-xl-4" id="p-footer2">
+        <p class="info-footer"><img src="../../images/tlf.svg" class="img-footer" id="icon2">653105506</p>
+    </div>
+    
+    <div class="col-xs-12 col-md-12 col-xl-4" id="p-footer3">
+        <p class="info-footer"><img src="../../images/sitemap.svg" class="img-footer" id="icon3">Mapa del sitio</p>
+    </div>
+</footer>
+
