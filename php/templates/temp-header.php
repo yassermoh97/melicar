41de19d341f1f7d1b774f60018db4c180380ce55
yasser <yasser@yass>
2019-05-19 21:58:27 +0200

    Creación de un botón colapsable para el menú de navegación.
    Declaración de un "div" para agrupar el contenido del menú de 
    navegación, para que colapse y se agrupe en el botón previamente
    declarado.
    
    Declaración de "<hr>" para dividir el contenido de "<select>".

--- php/templates/temp-header.php
+++ php/templates/temp-header.php
@@ -1,44 +1,52 @@
 <header>
     <div>
         <!-- Navigation bar -->
-        <nav class="navbar navbar-expand-sm">
+        <nav class="navbar navbar-expand-sm navbar-default">
             <!-- Brand -->
             <img src="../../images/logo.png" id="logotipo"><a class="navbar-brand" href="#"></a>
             
-            <!-- Links and Dropdown -->
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="nav-link " href="#" id="navbardrop1" data-toggle="dropdown">
-                        COCHES Y FURGONETAS
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">NUESTROS COCHES<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
-                        <a class="dropdown-item" href="#">NUESTRAS FURGONETAS<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
-                        <a class="dropdown-item" href="#">COCHES PRESTIGE<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
-                    </div>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="#" id="navbardrop2" data-toggle="dropdown">
-                        SERVICIOS
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">SERVICIOS<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
-                        <a class="dropdown-item" href="#">EXTRAS<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
-                    </div>
-                </li>
-            </ul>
+            <!-- Toggler/collapsibe Button -->
+            <!-- Toggler/collapsibe Button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <img src="../../images/menu-icon.png" id="menu-icon">
+            </button>
             
-            <!-- Link perfil -->
-            <div class="nav navbar-nav ml-auto" id="tuperfil">
+            <!-- Links and Dropdown -->
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link-perfil" href="#" id="nav-link3">
-                            TU PERFIL<span><img src="../../images/logo-user.svg" id="logo-user"></span>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="#" id="navbardrop1" data-toggle="dropdown">
+                            COCHES Y FURGONETAS
                         </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">NUESTROS COCHES<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
+                            <hr><a class="dropdown-item" href="#">NUESTRAS FURGONETAS<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
+                            <hr><a class="dropdown-item" href="#">COCHES PRESTIGE<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="#" id="navbardrop2" data-toggle="dropdown">
+                            SERVICIOS
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">SERVICIOS<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
+                            <hr><a class="dropdown-item" href="#">EXTRAS<span><img id="mark-red" src="../../images/mark-red.svg"></span></a>
+                        </div>
                     </li>
                 </ul>
-            </div>  
+
+                <!-- Link perfil -->
+                <div class="nav navbar-nav ml-auto" id="tuperfil">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link-perfil" href="#" id="nav-link3">
+                                TU PERFIL<span><img src="../../images/logo-user.svg" id="logo-user"></span>
+                            </a>
+                        </li>
+                    </ul>
+                </div> 
+            </div>
         </nav>
         
         <!-- Llamada a archivos -->
