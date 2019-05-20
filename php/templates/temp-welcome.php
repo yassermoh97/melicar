7c3be42994585909ce689579aebc772dff0df771
yasser <yasser@yass>
2019-05-20 22:36:18 +0200

    Corrección de error ortográfico.

--- php/templates/temp-welcome.php
+++ php/templates/temp-welcome.php
@@ -2,7 +2,7 @@
     <div class="col-xs-12 col-md-12 col-xl-12">
         <div class="card" id="welcome">
             <div class="card-body" id="body-welcome">
-                <h2 class="car-title" id="title-welcome">¿Quénes somos?</h2>
+                <h2 class="card-title" id="title-welcome">¿Quénes somos?</h2>
                 <p>Bienvenido a Melicar, líder en Melilla en alquiler de coches.
                 Melicar te garantiza un servicio insuperable, que busques coches económicos, familiares
                 o de alta gama para visitar nuestra ciudad, o bien furgonetas y vehículos comerciales.
