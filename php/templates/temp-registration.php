ce0106453e9b0618a40f1ff0dfeaffb8433a1171
yasser <yasser@yass>
2019-06-11 18:09:06 +0200

    Cambio de nombre de id y de name a todos los inputs del formulario de registro

--- php/templates/temp-registration.php
+++ php/templates/temp-registration.php
@@ -5,7 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        
+        <!-- Javascript -->
+        <script type="text/javascript" src="../../js/validar-signup.js"></script>
         
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="../../css/styles.css">
@@ -23,50 +28,56 @@
                     <h5 class="card-title" id="title-registration1">INFORMACIÓN PERSONAL</h5>
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-nombre" class="regist-control">Nombre*</label><br>
-                            <input type="text" id="regist-nombre" class="regist" name="regist-nombre">
+                            <label for="regist_nombre" class="regist-control">Nombre*</label><br>
+                            <input type="text" id="regist_nombre" class="regist" name="regist_nombre"><br>
+                            <div class="mensaje" id="mostrar_nombre"></div>
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-apellidos" class="regist-control">Apellidos*</label><br>
-                            <input type="text" id="regist-apellidos" class="regist" name="regist-apellidos">
+                            <label for="regist_apellidos" class="regist-control">Apellidos*</label><br>
+                            <input type="text" id="regist_apellidos" class="regist" name="regist_apellidos">
+                            <div class="mensaje" id="mostrar_apellidos"></div>
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-telefono" class="regist-control">Número de teléfono*</label><br>
-                            <input type="text" id="regist-telefono" class="regist" name="regist-telefono">
+                            <label for="regist_telefono" class="regist-control">Número de teléfono*</label><br>
+                            <input type="text" id="regist_telefono" class="regist" name="regist_telefono">
+                            <div class="mensaje" id="mostrar_telefono"></div>
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-email" class="regist-control">Dirección de correo electrónico*</label><br>
-                            <input type="text" id="regist-email" class="regist" name="regist-email">
+                            <label for="regist_email" class="regist-control">Dirección de correo electrónico*</label><br>
+                            <input type="text" id="regist_email" class="regist" name="regist_email">
+                            <div class="mensaje" id="mostrar_email"></div>
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for=" regist-usuario" class="regist-control">Nombre de usuario*</label><br>
-                            <input type="text" id="regist-usuario" class="regist" name="regist-usuario">
+                            <label for="regist_usuario" class="regist-control">Nombre de usuario*</label><br>
+                            <input type="text" id="regist_usuario" class="regist" name="regist_usuario">
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-password1" class="regist-control">Contraseña*</label><br>
-                            <input type="password" id="regist-password1" class="regist" name="regist-password1">
+                            <label for="regist_password1" class="regist-control">Contraseña*</label><br>
+                            <input type="password" id="regist_password1" class="regist" name="regist_password1"><br>
+                            <div class="mensaje" id="mostrar_password1"></div>
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-password2" class="regist-control">Confirmar contraseña*</label><br>
-                            <input type="password" id="regist-password2" class="regist" name="regist-password2">
+                            <label for="regist_password2" class="regist-control">Confirmar contraseña*</label><br>
+                            <input type="password" id="regist_password2" class="regist" name="regist_password2"><br>
+                            <div class="mensaje" id="mostrar_password2"></div>
                         </div>
                     </div>
                 </div>
@@ -79,45 +90,51 @@
                     <h5 class="card-title" id="title-registration2">DETALLES DE CONTACTO</h5>
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-pais" class="regist-control">País*</label><br>
-                            <input type="text" id="regist-pais" class="regist" name="regist-pais">
+                            <label for="regist_pais" class="regist-control">País*</label><br>
+                            <input type="text" id="regist_pais" class="regist" name="regist_pais"><br>
+                            <div class="mensaje" id="mostrar_pais"></div>
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-ciudad" class="regist-control">Ciudad*</label><br>
-                            <input type="text" id="regist-ciudad" class="regist" name="regist-ciudad">
+                            <label for="regist_ciudad" class="regist-control">Ciudad*</label><br>
+                            <input type="text" id="regist_ciudad" class="regist" name="regist_ciudad"><br>
+                            <div class="mensaje" id="mostrar_ciudad"></div>
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-calle" class="regist-control">Dirección*</label><br>
-                            <input type="text" id="regist-calle" class="regist" name="regist-calle">
+                            <label for="regist_calle" class="regist-control">Dirección*</label><br>
+                            <input type="text" id="regist_calle" class="regist" name="regist_calle"><br>
+                            <div class="mensaje" id="mostrar_calle"></div>
                         </div>
                     </div>
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-numero" class="regist-control">Número*</label><br>
-                            <input type="text" id="regist-numero" class="regist" name="regist-numero">
+                            <label for="regist_numero" class="regist-control">Número*</label><br>
+                            <input type="text" id="regist_numero" class="regist" name="regist_numero"><br>
+                            <div class="mensaje" id="mostrar_numero"></div>
                         </div>
                     </div>
                     
                     
                     <div class="row">  
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-puerta" class="regist-control">Puerta</label><br>
-                            <input type="text" id="regist-puerta" class="regist" name="regist-puerta">
+                            <label for="regist_puerta" class="regist-control">Puerta</label><br>
+                            <input type="text" id="regist_puerta" class="regist" name="regist_puerta"><br>
+                            <div class="mensaje" id="mostrar_puerta"></div>
                         </div>
                     </div>
                     
                     
                     <div class="row">
                         <div class="col-xs-12 col-md-12 col-xl-12">
-                            <label for="regist-postal" class="regist-control">Código postal*</label><br>
-                            <input type="text" id="regist-postal" class="regist" name="regist-postal">
+                            <label for="regist_postal" class="regist-control">Código postal*</label><br>
+                            <input type="text" id="regist_postal" class="regist" name="regist_postal"><br>
+                            <div class="mensaje" id="mostrar_postal"></div>
                         </div>
                     </div>
                 </div>
@@ -135,8 +152,7 @@
         
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+       
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     </body>
 </html>
