67cc07406a96fb8e9a7368d243a60734c0d8de3b
yasser <yasser@yass>
2019-06-11 17:33:16 +0200

    Archivo de alta de usuario a través de la clase Cliente

---
+++ php/clases/signup.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once 'cliente.php';
+
+# Datos recogidos del formulario de registro
+$nombre = $_POST['r_nombre'];
+$apellidos = $_POST['r_apellidos'];
+$telefono = $_POST['r_telefono'];
+$email = $_POST['r_email'];
+$usuario = $_POST['r_usuario'];
+$pass = $_POST['r_password'];
+$pais = $_POST['r_pais'];
+$ciudad = $_POST['r_ciudad'];
+$direccion = $_POST['r_direccion'];
+$numero = $_POST['r_numero'];
+$puerta = $_POST['r_puerta'];
+$postal = $_POST['r_codigo_postal'];
+
+$user = new Cliente();
+$register = $user->reg_cliente($nombre, $apellidos, $telefono, $email, $usuario, $pass, $pais, $ciudad, $direccion, $numero, $puerta, $postal);
