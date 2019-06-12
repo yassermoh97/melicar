89ab9cbd2ecdadc866706b89afa2355925f35886
yasser <yasser@yass>
2019-06-12 16:46:51 +0200

    Modificación en comentarios

--- php/templates/temp-login.php
+++ php/templates/temp-login.php
@@ -3,25 +3,17 @@
         <!-- User -->
         <div class="col-xs-12 col-md-12 col-xl-4">
             <p>
-                <input type="text" class="form-control" id="user" placeholder="Usuario">
+                <input type="text" class="form-control" id="user" name="user "placeholder="Usuario">
             </p> 
         </div>
         
         <!-- Password -->
         <div class="col-xs-12 col-md-12 col-xl-4">
             <p>
-                <input type="password" class="form-control" id="pass" placeholder="Contraseña">
+                <input type="password" class="form-control" id="pass" name="pass" placeholder="Contraseña">
             </p> 
         </div>
-        
-        <!-- Signin -->
-        <!--<div class="col-xs-12 col-md-12 col-xl-3">
-            <p>
-                <button type="button" class="btn" id="signin">INICIAR SESIÓN</button>
-            </p> 
-        </div>-->
-
-        <!-- Signup -->
+        <!-- Botones iniciar sesión y registrarse -->
         <div class="col-xs-12 col-md-12 col-xl-4">
             <div class="row">
                 <p class="col-xs-12 col-md-4 col-xl-6">
