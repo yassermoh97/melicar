77ea1b877acae64a1b776f66eb5b13bdbac7095d
yasser <yasser@yass>
2019-06-12 16:41:08 +0200

    Script para dar de alta a un nuevo cliente

--- php/clases/signup.php
+++ php/clases/signup.php
@@ -14,13 +14,22 @@ $apellidos = $_POST['r_apellidos'];
 $telefono = $_POST['r_telefono'];
 $email = $_POST['r_email'];
 $usuario = $_POST['r_usuario'];
-$pass = $_POST['r_password'];
+$pass = md5($_POST['r_password']);
 $pais = $_POST['r_pais'];
 $ciudad = $_POST['r_ciudad'];
 $direccion = $_POST['r_direccion'];
 $numero = $_POST['r_numero'];
-$puerta = $_POST['r_puerta'];
 $postal = $_POST['r_codigo_postal'];
 
 $user = new Cliente();
-$register = $user->reg_cliente($nombre, $apellidos, $telefono, $email, $usuario, $pass, $pais, $ciudad, $direccion, $numero, $puerta, $postal);
+$register = $user->reg_cliente($nombre, $apellidos, $telefono, $email, $usuario, $pass, $pais, $ciudad, $direccion, $numero, $postal);
+
+
+if (!($user == false)) {
+  
+    
+} else {
+    
+    echo "Error en el registro";
+    
+}
