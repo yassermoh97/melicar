53e794cf485732a268619f5579fb9eb71fb48db6
yasser <yasser@yass>
2019-06-12 16:44:52 +0200

    Creación de un modal para que aparezca por pantalla una advertencia al acceder a "Tu perfil" sin antes haber iniciado sesión.
    Declaración de una condición para que aparezcan o no, elementos de 
    control sobre la barra de navegación

--- php/templates/temp-header.php
+++ php/templates/temp-header.php
@@ -1,3 +1,30 @@
+<?php
+    // Iniciar sesión
+    session_start();
+
+    // Verificar que esta declarada la sesión y que su estado es autenticado.
+    if (isset($_SESSION["l_usuario"]) and $_SESSION["estado"] == "Autenticado") {
+        
+        $cerrarsesion = '<li class="nav-item">
+                            <button type="button" id="cerrarsesion" onclick="location.href=\'../clases/logout.php\'">CERRAR SESIÓN</button>
+                        </li>';
+        $tuperfil = '<li class="nav-item">
+                        <button type="button" class="bt-perfil" role="link" onclick="window.location=\'temp-perfil.php\'">
+                                TU PERFIL<span><img src="../../images/logo-user.svg" id="logo-user"></span>
+                        </button>
+                    </li>';
+        
+    } else {
+        
+        $cerrarsesion = '';
+        $tuperfil = '<li class="nav-item">
+                        <button type="button" class="bt-perfil" data-toggle="modal" data-target="#miModal">
+                                TU PERFIL<span><img src="../../images/logo-user.svg" id="logo-user"></span>
+                        </button>
+                    </li>';
+        
+    }
+?>
 <header>
     <div>
         <!-- Navigation bar -->
@@ -37,14 +64,24 @@
                 <!-- Link perfil -->
                 <div class="nav navbar-nav ml-auto" id="tuperfil">
                     <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link-perfil" href="#" id="nav-link3">
-                                TU PERFIL<span><img src="../../images/logo-user.svg" id="logo-user"></span>
-                            </a>
-                        </li>
+                        <?php echo $tuperfil; ?>
+                        <?php echo $cerrarsesion; ?>
                     </ul>
                 </div> 
             </div>
         </nav>   
     </div>
 </header>
+
+<div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                </button>
+                <h5 class="modal-title" id="myModalLabel">Para acceder a tu perfil, primero debes iniciar sesión.</h5>
+            </div>
+        </div>
+    </div>
+</div>
