b222bdbb597de5e62ed7789861441be7474a0241
yasser <yasser@yass>
2019-06-12 16:48:10 +0200

    Creación de una barra de navegación vertical para seccion "tu perfil"

---
+++ php/templates/temp-navertical.php
@@ -0,0 +1,16 @@
+<div class="container-config">
+    <h2 class="title-config">AJUSTES DE PERFIL</h2>
+    <ul class="nav flex-column">
+        <li class="nav-config">
+            <a class="nav-config" href="temp-updateinfo.php">Mi cuenta</a>
+        </li>
+
+        <li class="nav-config">
+            <a class="nav-config" href="temp-updatepass.php">Modificar contraseña</a>
+        </li>
+
+        <li class="nav-config">
+            <a class="nav-config" href="#">Reservas</a>
+        </li>
+    </ul>
+</div>
