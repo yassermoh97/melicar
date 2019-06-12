ff2bbdc59d2adc7b11e42cb35f7cb50d8efb277e
yasser <yasser@yass>
2019-06-12 16:46:11 +0200

    Declaración de session_start().
    Declaración de una condición para que aparezcan o no, el formulario
    login. Dependiendo de si la sesión está iniciada o no.

--- php/templates/temp-index.php
+++ php/templates/temp-index.php
@@ -1,3 +1,46 @@
+<?php
+    // Iniciar sesión
+    session_start();
+
+    // Verificar que esta declarada la sesión y que su estado es autenticado.
+    if (isset($_SESSION["l_usuario"]) and $_SESSION["estado"] == "Autenticado") {
+        
+        $iniciar = '';
+      
+    } else {
+        
+        $iniciar = '<div class="row" id="login">
+                        <div class="row" id="login-content">
+                            <!-- User -->
+                            <div class="col-xs-12 col-md-12 col-xl-4">
+                                <p>
+                                    <input type="text" class="form-control" id="user" name="user" placeholder="Usuario">
+                                </p> 
+                            </div>
+
+                            <!-- Password -->
+                            <div class="col-xs-12 col-md-12 col-xl-4">
+                                <p>
+                                    <input type="password" class="form-control" id="pass" name="pass" placeholder="Contraseña">
+                                </p> 
+                            </div>
+                            <!-- Botones iniciar sesión y registrarse -->
+                            <div class="col-xs-12 col-md-12 col-xl-4">
+                                <div class="row">
+                                    <p class="col-xs-12 col-md-4 col-xl-6">
+                                        <button type="button" class="btn" id="signin">INICIAR SESIÓN</button>
+                                    </p> 
+
+                                    <p class="col-xs-12 col-md-4 col-xl-6">
+                                         <button type="button" class="btn" id="signup" onclick="location.href=\'temp-registration.php\'">REGISTRARSE</button>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>'; 
+        
+    }
+?>
 <html>
     <head>
         <!-- Required meta tags -->
@@ -5,7 +48,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        
+         <!-- Javascript -->
+         <script type="text/javascript" src="../../js/validar-signin.js"></script>
         
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="../../css/styles.css">
@@ -15,18 +63,14 @@
     <body>
         <!-- Llamada a archivos -->
         <?php include 'temp-header.php'; ?>
-        <?php include 'temp-login.php'; ?>
+        <?php echo $iniciar; ?>
         <?php include 'temp-search.php'; ?>
         <?php include 'temp-welcome.php'; ?>
         <?php include 'temp-footer.php'; ?>
         
-        
-        
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     </body>
 </html>
 
