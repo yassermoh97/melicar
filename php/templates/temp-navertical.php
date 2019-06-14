4d934fe35aa44d08adae9142778159b2fb16a88f
yasser <yasser@yass>
2019-06-14 18:07:02 +0200

    Creación de la plantilla para el panel de control del cliente

--- php/templates/temp-navertical.php
+++ php/templates/temp-navertical.php
@@ -1,16 +1,35 @@
-<div class="container-config">
-    <h2 class="title-config">AJUSTES DE PERFIL</h2>
-    <ul class="nav flex-column">
-        <li class="nav-config">
-            <a class="nav-config" href="temp-updateinfo.php">Mi cuenta</a>
-        </li>
+<div>
+    <!-- Llamada a archivos -->
+    <?php include 'temp-header.php'; ?>
 
-        <li class="nav-config">
-            <a class="nav-config" href="temp-updatepass.php">Modificar contraseña</a>
-        </li>
+    <?php
+        session_start();
+    ?>
 
-        <li class="nav-config">
-            <a class="nav-config" href="#">Reservas</a>
-        </li>
-    </ul>
+    <div class="row">
+        <div class="config col-xs-12 col-md-2 col-xl-2">
+            <div class="container-config">
+                <h2 class="title-config">AJUSTES DE PERFIL</h2>
+                <ul class="nav flex-column">
+                    <li class="nav-config">
+                        <a class="nav-config" href="temp-updateinfo.php">Mi cuenta</a>
+                    </li>
+
+                    <li class="nav-config">
+                        <a class="nav-config" href="temp-updatepass.php">Modificar contraseña</a>
+                    </li>
+
+                    <li class="nav-config">
+                        <a class="nav-config" href="#">Reservas</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-md-10 col-xl-10">
+            
+        </div>
+    </div>
+
+    <?php include 'temp-footer.php'; ?>
 </div>
