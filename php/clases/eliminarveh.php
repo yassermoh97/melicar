36282d2f60939d70d4b9c1e8ffaf38d9f889cb95
yasser <yasser@yass>
2019-06-17 01:58:14 +0200

    Creación de mapa del sitio

--- php/clases/eliminarveh.php
+++ php/clases/eliminarveh.php
@@ -2,14 +2,16 @@
 
 require_once 'administrador.php';
 
-# Datos recogidos del formulario de registro
+// Enviados a través de Ajax
 $id = $_POST['idveh'];
 
-
+// Creación del objeto y almacenamiento en una variable
 $car1 = new Administrador();
+
+// Ejecutar el método de la clase
 $register = $car1->elim_vehiculo($id);
 
-
+// Mensajes de error
 if (!($car1 == false)) {
   
     
