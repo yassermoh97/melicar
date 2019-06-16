1542c6ab2563a7fcdadf22c65ba4a5c8cfc91ceb
yasser <yasser@yass>
2019-06-17 01:55:41 +0200

    Código comentado

--- php/templates/temp-advertical.php
+++ php/templates/temp-advertical.php
@@ -1,10 +1,20 @@
 <div>
+    <!-- Archivos incluidos -->
     <?php include 'temp-header.php'; ?>
+    
+    <!-- Migas de pan -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="temp-index.php">Inicio</a></li>
+            <li class="breadcrumb-item" aria-current="page">Tu perfil</a></li>
+        </ol>
+    </nav>
     
     <?php
         session_start();
     ?>
     
+    <!-- Panel de control del administrador -->
     <div class="row">
         <div class="config col-xs-12 col-md-2 col-xl-2">
             <div class="container-config">
@@ -15,7 +25,7 @@
                     </li>
 
                     <li class="nav-config">
-                        <a class="nav-config" href="temp-vehiculosreg">Vehículos registrados</a>
+                        <a class="nav-config" href="temp-vehiculosreg.php">Vehículos registrados</a>
                     </li>
 
                     <li class="nav-config">
@@ -29,6 +39,6 @@
             
         </div>
     </div>
-
+    <!-- Archivos incluidos -->
     <?php include 'temp-footer.php'; ?>
 </div> 
