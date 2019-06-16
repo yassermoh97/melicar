12eb3bd3950e001653a0837dbe6daa94e213abfb
yasser <yasser@yass>
2019-06-16 14:27:22 +0200

    Declaración de un enlace a javascript

--- php/templates/temp-index.php
+++ php/templates/temp-index.php
@@ -54,6 +54,7 @@
         
          <!-- Javascript -->
          <script type="text/javascript" src="../../js/validar-signin.js"></script>
+         <script type="text/javascript" src="../../js/busqueda.js"></script>
         
         <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="../../css/styles.css">
