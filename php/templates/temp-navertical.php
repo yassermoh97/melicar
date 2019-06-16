6c5aaacf8c5ed128a7f6dcfdf27cb32fce5ac7e7
yasser <yasser@yass>
2019-06-16 16:45:36 +0200

    Declaración del enlace que redirige a la página que visualiza las reservas realizadas

--- php/templates/temp-navertical.php
+++ php/templates/temp-navertical.php
@@ -20,7 +20,7 @@
                     </li>
 
                     <li class="nav-config">
-                        <a class="nav-config" href="#">Reservas</a>
+                        <a class="nav-config" href="temp-reservasreg.php">Reservas</a>
                     </li>
                 </ul>
             </div>
