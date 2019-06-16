709237add6e0bd8bbc11db97a5740cd73be64398
yasser <yasser@yass>
2019-06-17 01:54:41 +0200

    Código comentado

--- js/eliminar-reserva.js
+++ js/eliminar-reserva.js
@@ -1,10 +1,14 @@
+// Función para enviar el id de la reserva que deseamos eliminar
 function envio(id) {
+    // Recolección de datos a través del id
     var reservaid = $("#reservaid" + id).val();
     
+    // Almacenamiento en una variable
     var datos = {
         "idres" : reservaid
     };
     
+    // Llamada a Ajax
     $.ajax({
         data: datos,
         type: "POST",
