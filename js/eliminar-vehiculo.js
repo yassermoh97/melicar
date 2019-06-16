8a9c86f5fd96dbf647647ff35dd90ca2b4ccd9cc
yasser <yasser@yass>
2019-06-17 01:54:47 +0200

    Código comentado

--- js/eliminar-vehiculo.js
+++ js/eliminar-vehiculo.js
@@ -1,23 +1,27 @@
+// Función para enviar el id del vehículo que queremos eliminar
 function enviar(id) {
+    // Recolección de datos a través del id
     var vehiculoid = $("#vehiculo_id" + id).val();
     
+    // Almacenamiento en una variable
     var datos = {
         "idveh" : vehiculoid
     };
     
+    // Llamada a Ajax
     $.ajax({
         data: datos,
         type: "POST",
         url: "../clases/eliminarveh.php",
         success: function(response) {
             if (!response) {
-                
-            } else {
                 alert("Has eliminado un vehículo del registro");
-                window.location.replace("temp-vehiculosreg.php")
+                window.location.replace("temp-vehiculosreg.php");
+            } else {
+                
             }
         }
-    })
+    });
 }
 
 
