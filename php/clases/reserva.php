387858b54048da0c33a9cb5ef2ff96c0ef9e3f09
yasser <yasser@yass>
2019-06-17 01:58:37 +0200

    Creación de mapa del sitio

--- php/clases/reserva.php
+++ php/clases/reserva.php
@@ -2,7 +2,7 @@
 
 require_once 'cliente.php';
 
-# Datos recogidos del formulario de registro
+// Enviados a través de Ajax
 $cli = $_POST['cli_id'];
 $veh = $_POST['veh_id'];
 $fecha_en = $_POST['en_fecha'];
@@ -10,8 +10,12 @@ $fecha_de = $_POST['en_hora'];
 $hora1_en = $_POST['de_fecha'];
 $hora2_de = $_POST['de_hora'];
 
+// Creación del objeto y almacenamiento en una variable
 $user = new Cliente();
+
+// Ejecutar el método de la clase
 $register = $user->reservar($cli, $veh, $fecha_en, $hora1_en, $fecha_de, $hora2_de);
 
+// Redirigir a la página de inicio
 header("Location: ../templates/temp-index.php");
 
