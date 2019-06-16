870cd135c16586e9f2865fcee2d721e403fc8467
yasser <yasser@yass>
2019-06-16 14:30:18 +0200

    Creación de un formulario para el cuadro de búsqueda.

--- php/templates/temp-search.php
+++ php/templates/temp-search.php
@@ -1,47 +1,27 @@
 <div class="row" id="back">
     <div class="row" id="search">
-        <div id="search-content">
-            <div class="row">
-                <div class="col-xs-12 col-md-12 col-xl-12">
-                    <p class="search-control">Oficina de recogida</p>
-                    <select id="points1">
-                        <option value="vacio"></option>
-                        <option value="aeropuerto">Aeropuerto de Melilla(MLN)-España</option>
-                        <option value="puerto">Puerto de Melilla-España</option>
-                    </select>
+        <form action="temp-alquilar.php" method="POST">
+            <div id="search-content">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6 col-xl-6">
+                        <label for="date1" class="search-control">Fecha de recogida</label><br>
+                        <input type="date" class="option" id="option-date1" name="option-date1" required>
+                        <input type="time" class="option" id="option-time1" name="option-time1" required>    
+                    </div>
+
+                    <div class="col-xs-12 col-md-6 col-xl-6">
+                        <label for="date2" class="search-control">Fecha de devolución</label><br>
+                        <input type="date" class="option" id="option-date2" name="option-date2" required>
+                        <input type="time" class="option" id="option-time2" name="option-time2" required>     
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-12 col-md-12 col-xl-12">
+                        <input type="submit" class="boton" id="search-btn" name="search-btn" value="BUSCAR">
+                    </div>
                 </div>
             </div>
-            
-            <div class="row">
-                <div class="col-xs-12 col-md-12 col-xl-12">
-                    <p class="search-control">Oficina de devolución</p>
-                    <select id="points2">
-                        <option value="vacio"></option>
-                        <option value="aeropuerto">Aeropuerto de Melilla(MLN)-España</option>
-                        <option value="puerto">Puerto de Melilla-España</option>
-                    </select><br>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col-xs-12 col-md-6 col-xl-6">
-                    <label for="date1" class="search-control">Fecha de recogida</label><br>
-                    <input type="date" class="option">
-                    <input type="time" class="option">    
-                </div>
-                
-                <div class="col-xs-12 col-md-6 col-xl-6">
-                    <label for="date2" class="search-control">Fecha de devolución</label><br>
-                    <input type="date" class="option">
-                    <input type="time" class="option">    
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col-xs-12 col-md-12 col-xl-12">
-                    <button type="button" class="boton" id="search-btn">BUSCAR COCHES<span><img id="mark-white" src="../../images/mark-white.svg"></span></button>
-                </div>
-            </div>
-        </div>
+        </form>
     </div>
 </div>
