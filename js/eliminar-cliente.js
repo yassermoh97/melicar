9409e967b78016fe7213cb17c90c5689a1f84afc
yasser <yasser@yass>
2019-06-17 01:54:35 +0200

    Código comentado

--- js/eliminar-cliente.js
+++ js/eliminar-cliente.js
@@ -1,10 +1,14 @@
+// Función para enviar el id del cliente que queremos eliminar a eliminarcli.php
 function enviar(id) {
+    // Recolección del dato a través de su id
     var clienteid = $("#cliente_id" + id).val();
     
+    // Almacenamiento de dato en una variable
     var datos = {
         "idcli" : clienteid
     };
     
+    // Llamada a Ajax
     $.ajax({
         data: datos,
         type: "POST",
