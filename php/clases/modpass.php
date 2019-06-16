16f8478e694296120739126c66bdaedeb17c4ac6
yasser <yasser@yass>
2019-06-17 01:58:31 +0200

    Creación de mapa del sitio

--- php/clases/modpass.php
+++ php/clases/modpass.php
@@ -2,14 +2,17 @@
 
 require_once 'cliente.php';
 
-# Datos recogidos del formulario de registro
+// Enviados a través de Ajax
 $us = $_POST['m_usuario'];
 $pass1 = md5($_POST['m_pass']);
 
+// Creación del objeto y almacenamiento en una variable
 $user = new Cliente();
+
+// Ejecutar el método de la clase
 $register = $user->mod_pass($us, $pass1);
 
-
+// Mensajes de error
 if (!($user == false)) {
   
     
